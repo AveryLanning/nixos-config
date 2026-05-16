436ce888ca9819f46fc28804a3140764377ddb06
Avery Lanning <avery@lanning.org>
2026-05-16 09:16:40 -0500

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS config with Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.avery = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
